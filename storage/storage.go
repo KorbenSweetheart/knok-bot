463e038eaf2f789eaf3ece08489b968c81ef1aa2
Ivan Andreev <ivan.joker88@gmail.com>
2025-11-09 23:35:54 +0200

    feat: add storage interface and methods to cover storage management

---
+++ storage/storage.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"read-reminder-bot/lib/e"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(userName string) (*Page, error)
+	Remove(p *Page) error
+	IsExists(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL      string
+	UserName string // who send it
+	// Created time.Time // if we want to improve and send old or fresh articles
+}
+
+func (p Page) Hash() (string, error) {
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", e.Wrap("can't calculate hash", err)
+	}
+
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", e.Wrap("can't calculate hash", err)
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
